6bc141351ce5d7f773eb1b2af7e19dd6f4b67070
404Nikhil <nicksd111@gmail.com>
2024-06-21 00:19:36 +0530

    states

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,18 @@
 'use client'
 
-import { useSession, signIn, signOut } from "next-auth/react"
-
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z  from "zod"
+import Link from "next/link"
+import { useState } from "react"
+const page = () => {
+  const [username, setUsername] = useState('')
+  const [usernameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState('false')
+  const [isSubmitting, setIsSubmitting] = useState(false)
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-orange-500 px-3 py-4 m-4 rounded" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>page</div>
   )
 }
+
+export default page
