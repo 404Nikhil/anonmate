ee7df4fc14d4d3ca089a1105dbf991de826ed573
404Nikhil <nicksd111@gmail.com>
2024-06-21 00:26:57 +0530

    debouncing and toaster setup

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,17 +1,21 @@
 'use client'
-
+import { useDebounceValue } from 'usehooks-ts'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z  from "zod"
 import Link from "next/link"
 import { useState } from "react"
 const page = () => {
-  const [username, setUsername] = useState('')
-  const [usernameMessage, setUsernameMessage] = useState('')
-  const [isCheckingUsername, setIsCheckingUsername] = useState('false')
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [username, setUsername] = useState('') // field
+  const [usernameMessage, setUsernameMessage] = useState('') // request from backend
+  const [isCheckingUsername, setIsCheckingUsername] = useState('false') // loader state,  boolean
+  const [isSubmitting, setIsSubmitting] = useState(false) // form state response, boolean
+  const debouncedUsername = useDebounceValue(username, 300)
+  
   return (
     <div>page</div>
+    // hook
+
   )
 }
 
