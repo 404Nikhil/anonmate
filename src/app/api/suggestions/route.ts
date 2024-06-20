3ee49e88c8a06ed8de365ca3f72f0203db13a294
404Nikhil <nicksd111@gmail.com>
2024-06-20 22:58:38 +0530

    stream response sdk

--- src/app/api/suggestions/route.ts
+++ src/app/api/suggestions/route.ts
@@ -1,38 +1,34 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText, streamToResponse } from 'ai';
 import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { StreamingTextResponse } from 'ai';
 import { NextResponse } from 'next/server';
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-export const runtime = 'edge';
+import { OpenAICompletionLanguageModel } from '@ai-sdk/openai/internal';
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  try {
-    const prompt =
-      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+try {
+   const prompt = 
+   "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
-    const response = await openai.completions.create({
-      model: 'gpt-3.5-turbo-instruct',
-      max_tokens: 400,
-      stream: true,
-      prompt,
-    });
+   const response = await streamText({
+    model: openai('gpt-3.5-turbo-instruct'),
+    maxTokens: 200,
+    prompt,
+  });
 
-    const stream = OpenAIStream(response);
-    
-    
-    return new StreamingTextResponse(stream);
-  } catch (error) {
-    if (error instanceof OpenAI.APIError) {
-      // OpenAI API error handling
-      const { name, status, headers, message } = error;
-      return NextResponse.json({ name, status, headers, message }, { status });
+    return response.toAIStreamResponse();
+
+} catch (error) {
+    if(error instanceof OpenAI.APIError) {
+        const {name, status, headers, message} = error
+        return NextResponse.json({
+            name, status, headers, message
+        }, {status})
     } else {
-      // General error handling
-      console.error('An unexpected error occurred:', error);
-      throw error;
+        console.error("An unexpected error has occured", error)
+        throw error
     }
-  }
+}
 }
